e12b9e8e5caff6b141e110dbd409dbab3601bd80
youngjunkim <kyj9168@gmail.com>
2021-07-14 23:03:45 +0900

    iframe 변경

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -4,8 +4,15 @@ const Chat = (props) => {
     return (
         <div className={props.sidebar ? "chat" : "chat active"}>
             <iframe
-                src="http://kyj9168.ddns.net:6101/"
-                style={{ width: "100%", height: "100%" ,border:"0px"}}
+                src="http://192.168.1.146:6101/"
+                style={{
+                    width: "100%",
+                    height: "100%",
+                    border: "0px",
+                    position: "absolute",
+                    right: 0,
+                    bottom: 0,
+                }}
             />
         </div>
     );
