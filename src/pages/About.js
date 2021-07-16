ab3a51b05142ebb260b95ace6f6f203d0c8d9106
youngjunkim <kyj9168@gmail.com>
2021-07-16 15:00:22 +0900

    about 연동

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,7 @@ const About = (props) => {
     return (
         <div className={props.sidebar ? "about" : "about active"}>
             <iframe
-                src="http://192.168.1.146:6102/"
+                src="http://kyj9168.ddns.net:6102/"
                 style={{
                     width: "100%",
                     height: "100%",
