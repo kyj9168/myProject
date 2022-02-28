8ca3f54796e76dee3c563eed961c20911ad5a244
youngjunkim <kyj9168@gmail.com>
2022-03-01 02:52:19 +0900

    수정

--- server/routes/index.js
+++ server/routes/index.js
@@ -12,11 +12,36 @@ const getIp = (req) => {
 // router.post('/dialogue', apiController.dialogue);
 router.get('/AutoCom', apiController.AutoCom);
 
-router.get('*', (req, res, next) => {
-   let md = new MobileDetect(req.headers['user-agent']);
-    console.log(`TIME:[${moment().format('yyyy-HH-mm hh:mm:ss')} ] IP:[${getIp(req)}] BROWSER:[${md.userAgent()}] OS:[${md.os()}] PHONE:[${md.mobile()}]`);
+router.get('/', (req, res, next) => {
+    let md = new MobileDetect(req.headers['user-agent']);
+    console.log(
+        `TIME:[${moment().format('yyyy-HH-mm hh:mm:ss')} ] IP:[${getIp(
+            req
+        )}] BROWSER:[${md.userAgent()}] OS:[${md.os()}] PHONE:[${md.mobile()}]`
+    );
 
     res.sendFile(approot + '/build/index.html');
 });
 
+router.get('/chat', (req, res, next) => {
+    let md = new MobileDetect(req.headers['user-agent']);
+    console.log(
+        `TIME:[${moment().format('yyyy-HH-mm hh:mm:ss')} ] IP:[${getIp(
+            req
+        )}] BROWSER:[${md.userAgent()}] OS:[${md.os()}] PHONE:[${md.mobile()}]`
+    );
+
+    res.sendFile(approot + '/build/index.html');
+});
+
+router.get('/Diary', (req, res, next) => {
+    let md = new MobileDetect(req.headers['user-agent']);
+    console.log(
+        `TIME:[${moment().format('yyyy-HH-mm hh:mm:ss')} ] IP:[${getIp(
+            req
+        )}] BROWSER:[${md.userAgent()}] OS:[${md.os()}] PHONE:[${md.mobile()}]`
+    );
+
+    res.sendFile(approot + '/build/index.html');
+});
 module.exports = router;
