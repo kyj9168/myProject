47986828b80ac070975d433a88fd30ad5b4e8d6d
youngjunkim <kyj9168@gmail.com>
2022-03-08 15:53:36 +0900

    router 수정

--- server/index.js
+++ server/index.js
@@ -2,32 +2,47 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
-const configfile = require("../.config/config.json");
+const configfile = require('../.config/config.json');
 const runmode = configfile.runmode;
 const config = configfile[runmode];
 const basicAuth = require('express-basic-auth');
+const moment = require('moment');
 
-const indexRouter = require('./routes/index');
+const MobileDetect = require('mobile-detect');
+// const indexRouter = require('./routes/index');
 const port = config.EXPRESS_PORT;
-
+const getIp = (req) => {
+    return req.headers['x-forwarded-for']?.split(',').shift() || req.socket?.remoteAddress;
+};
 app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-)
-app.use(bodyParser.json())
-
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+app.use(bodyParser.json());
 
 // app.use(basicAuth({
 //   users: { 'admin': 'admin123!' },
 //   challenge: true
 // }))
-app.use('/', indexRouter);
-app.use('/', express.static(path.resolve(__dirname, '../build')));
-app.use('/', express.static(path.resolve(__dirname, '../public')));
+// app.use( indexRouter);
+// app.use( express.static(path.resolve(__dirname, '../build')));
+// app.use( express.static(path.resolve(__dirname, '../public')));
 
+// Serve static assets
 
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
+
+app.get('*', (req, res) => {
+    let md = new MobileDetect(req.headers['user-agent']);
+    console.log(
+        `TIME:[${moment().format('yyyy-HH-mm hh:mm:ss')} ] IP:[${getIp(
+            req
+        )}] BROWSER:[${md.userAgent()}] OS:[${md.os()}] PHONE:[${md.mobile()}]`
+    );
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+});
 
 app.listen(port, function () {
-  console.log(`listening on port ${port}!`);
+    console.log(`listening on port ${port}!`);
 });
--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
-import "./App.css";
-import Navbar from "./components/navbar";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Chat from "./pages/Chat";
-import Diary from "./pages/Diary";
-import Game from "./pages/Game";
-import Team from "./pages/Team";
-import Messages from "./pages/Messages";
-import About from "./pages/About";
+import React, { useState } from 'react';
+import './App.css';
+import Navbar from './components/navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Chat from './pages/Chat';
+import Diary from './pages/Diary';
+import Game from './pages/Game';
+import Team from './pages/Team';
+import Messages from './pages/Messages';
+import About from './pages/About';
 
 const App = () => {
     const [sidebar, setSidebar] = useState(true);
@@ -24,23 +24,10 @@ const App = () => {
                     setSelectMenu={setSelectMenu}
                 />
                 <Switch>
-                    <Route
-                        path="/"
-                        exact
-                        render={() => <Home sidebar={sidebar} />}
-                    />
-                    <Route
-                        path="/chat"
-                        render={() => <Chat sidebar={sidebar} />}
-                    />
-                    <Route
-                        path="/diary"
-                        render={() => <Diary sidebar={sidebar} />}
-                    />
-                        <Route
-                        path="/game"
-                        render={() => <Game sidebar={sidebar} />}
-                    />
+                    <Route path="/" exact render={() => <Home sidebar={sidebar} />} />
+                    <Route path="/chat" render={() => <Chat sidebar={sidebar} />} />
+                    <Route path="/diary" render={() => <Diary sidebar={sidebar} />} />
+                    <Route path="/game" render={() => <Game sidebar={sidebar} />} />
                 </Switch>
             </Router>
         </>
